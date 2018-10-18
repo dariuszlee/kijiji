e76271c767dd297d889ebb83e1c479c64584b9b0
dariuszlee <dariuszlee93@gmail.com>
2018-10-18 15:23:27 +0200

    Adding react

--- kijiji_auto_web/Gulpfile.js
+++ kijiji_auto_web/Gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
+var babel = require('gulp-babel')
 
 gulp.task('styles', function() {
     gulp.src('spa/scss/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./spa/css/'));
+        .pipe(gulp.dest('./spa/static/'));
 });
+
+gulp.task('watch', function() {
+    gulp.watch('spa/scss/*', ['styles'])
+    gulp.watch('spa/js/*', ['react'])
+});
+
+gulp.task('react', function(){
+    gulp.src('spa/js/*.jsx')
+        .pipe(babel({
+            plugins: ['transform-react-jsx']
+        }))
+        .pipe(gulp.dest('spa/static'));
+});
+
+gulp.task('default', ['watch']);
